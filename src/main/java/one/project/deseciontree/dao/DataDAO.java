1553946651a2cf414d258e24fc1171558d8ff3f0
safinAyah <ayah.safeen@gmail.com>
2019-11-30 20:53:32 +0200

    Entropy calculations are correct

--- src/main/java/one/project/deseciontree/dao/DataDAO.java
+++ src/main/java/one/project/deseciontree/dao/DataDAO.java
@@ -8,6 +8,7 @@ package one.project.deseciontree.dao;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 import java.util.function.Consumer;
 import one.project.deseciontree.model.ClassifiedFeature;
 import one.project.deseciontree.model.DataSet;
@@ -21,57 +22,74 @@ import weka.core.converters.ArffLoader.ArffReader;
  */
 public class DataDAO {
 
-  /**
-   * Reads a file and converts it into a dataset
-   *
-   * @param filePath the path of the file to read
-   * @return a dataset
-   * @throws IOException
-   */
-  public static DataSet readFile(String filePath) throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(filePath));
-    ArffReader arffFile = new ArffReader(reader);//read the arff file
-    Instances data = arffFile.getData();//extract the datset
-    data.setClassIndex(data.numAttributes() - 1);//set the class to be the last attribute
-    return convert(data);
-  }
-
-  /**
-   * A Consumer class that converts feature Instances to a dataset
-   */
-  private static class InstanceConsumer implements Consumer<Instance> {
-
-    private final DataSet dataSet;
-
-    public InstanceConsumer(DataSet dataSet) {
-      this.dataSet = dataSet;
+    /**
+     * Reads a file and converts it into a dataset
+     *
+     * @param filePath the path of the file to read
+     * @return a dataset
+     * @throws IOException
+     */
+    public static DataSet readFile(String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        ArffReader arffFile = new ArffReader(reader);//read the arff file
+        Instances data = arffFile.getData();//extract the datset
+        data.setClassIndex(data.numAttributes() - 1);//set the class to be the last attribute
+        return convert(data);
     }
 
-    @Override
-    public void accept(Instance featureInstance) {
-      ClassifiedFeature classifiedFeature = new ClassifiedFeature();
-      double[] featureINstanceAttributes = featureInstance.toDoubleArray();
-      for (int i = 0; i < featureINstanceAttributes.length; i++) {
-        if (i != featureInstance.classIndex()) {
-          classifiedFeature.getFeatureVector().add(String.valueOf(featureINstanceAttributes[i]));
-        } else {
-          classifiedFeature.setClassifedClass(String.valueOf(featureINstanceAttributes[i]));
+    public static Instances read(String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        ArffReader arffFile = new ArffReader(reader);//read the arff file
+        Instances data = arffFile.getData();//extract the datset
+        data.setClassIndex(data.numAttributes() - 1);//set the class to be the last attribute
+        return data;
+    }
+
+    /**
+     * A Consumer class that converts feature Instances to a dataset
+     */
+    private static class InstanceConsumer implements Consumer<Instance> {
+
+        int feature = 0;
+
+        private final DataSet dataSet;
+
+        public InstanceConsumer(DataSet dataSet) {
+            this.dataSet = dataSet;
         }
-      }
-      this.dataSet.getData().add(classifiedFeature);
+
+        @Override
+        public void accept(Instance featureInstance) {
+            ClassifiedFeature classifiedFeature = new ClassifiedFeature();
+            double[] featureINstance = featureInstance.toDoubleArray();
+            //System.out.println("lengthhh   ::   "+featureInstance.classIndex());
+            for (int i = 0; i < featureINstance.length; i++) {
+              
+                if (i != featureInstance.classIndex()) {
+                    classifiedFeature.getFeatureVector().add(String.valueOf(featureINstance[i]));
+                } else {
+                    classifiedFeature.setClassifedClass(String.valueOf(featureINstance[i]));
+//                    System.out.println("claaaaaaaaaaas :    "+classifiedFeature.getClassifedClass());
+                }
+            }
+            for (int i = 0; i < featureInstance.numAttributes(); i++) {
+                classifiedFeature.setName(featureInstance.attribute(i).name());
+
+            }
+            this.dataSet.getData().add(classifiedFeature);
+        }
+
     }
 
-  }
-
-  /**
-   * Converts a Weka dataset into a dataset for our use
-   *
-   * @param data the data to convert
-   * @return a dataset with the converted data
-   */
-  private static DataSet convert(Instances data) {
-    DataSet dataSet = new DataSet();
-    data.stream().forEach(new InstanceConsumer(dataSet));//iterate over the data and convert it
-    return dataSet;
-  }
+    /**
+     * Converts a Weka dataset into a dataset for our use
+     *
+     * @param data the data to convert
+     * @return a dataset with the converted data
+     */
+    private static DataSet convert(Instances data) {
+        DataSet dataSet = new DataSet();
+        data.stream().forEach(new InstanceConsumer(dataSet));//iterate over the data and convert it'
+        return dataSet;
+    }
 }
