925c063a8b4575ce45238ceef96c493f2837a060
safinAyah <ayah.safeen@gmail.com>
2020-01-04 00:30:45 +0200

    entropy method must be checked

--- src/main/java/one/project/deseciontree/decesion/tree/EntropyCalcuations.java
+++ src/main/java/one/project/deseciontree/decesion/tree/EntropyCalcuations.java
@@ -13,47 +13,76 @@ import one.project.deseciontree.model.FrequencyTable;
  */
 public class EntropyCalcuations {
 
-    public double calculateAttributeEntopy(FrequencyTable fTable) {
-      int [][]  freqTable = fTable.getfTable(); 
-        for(int i=0 ; i<freqTable.length;i++){
-            if (freqTable[0][i]==0 || freqTable[1][i]==0){
-                return 0 ; 
+    /**
+     *
+     *
+     * @param fTable
+     * @param totalData
+     * @return
+     */
+    public double calculateAttributeEntopy(FrequencyTable fTable, int totalData) {
+
+        double entropy = 0.0;
+        int featureAtrributeOne = 0;
+        int fratureAttributeTwo = 0;
+
+        int[][] freqTable = fTable.getfTable();
+        for (int i = 0; i < freqTable.length; i++) {
+//          
+            if (freqTable[i][0] == 0 || freqTable[i][1] == 0) {
+
+                entropy = 0;
+
+            } else if (freqTable[i][0] == freqTable[i][1]) {
+
+                entropy = 1;
+
+            } else {
+
+                int poisonTotal = 0;
+                int edable = 0;
+
+                poisonTotal += freqTable[i][1];
+                edable += freqTable[i][0];
+
+                featureAtrributeOne = freqTable[i][0];
+                fratureAttributeTwo = freqTable[i][1];
+
+                entropy = calculateP(freqTable[i][2], totalData) * (calculateFeatureEntropy(featureAtrributeOne, freqTable[i][2]) + calculateFeatureEntropy(fratureAttributeTwo, freqTable[i][2]));
                 
-            }else if(freqTable[0][i]==freqTable[1][i]){
-                return 1; 
-                            
-            }else{
-                int poisonTotal = freqTable[i][1]++; 
-                int ediable = freqTable[i][0]++; 
-                int total = freqTable[i][2]++; 
-                
-//                return (calculateFeatureEntropy(freqTable[i][2], i))
-            
-            
             }
-        
-        
+
+            entropy += entropy;
         }
-        
-        return 0;
+
+        return entropy;
     }
-    
-/**
- * 
- * 
- * @param attributeTotal
- * @param total
- * @return current entropy divided by two 
- */
-    public double calculateFeatureEntropy(int attributeTotal, int total){
-        double currentEntropy = Math.log(attributeTotal/(double)total)/Math.log(2);
-    
-        
-        return currentEntropy; 
+
+    /**
+     *
+     * @param featureTotal
+     * @param total
+     * @return
+     */
+    private double calculateP(int featureTotal, int total) {//checked   
+        double p = (double) featureTotal / (double) total;
+        return (double) featureTotal / (double) total;
+
     }
-    
- 
-    
-    
+
+    /**
+     *
+     *
+     * @param attributeTotal
+     * @param total
+     * @return current entropy divided by two
+     */
+    private double calculateFeatureEntropy(int attributeTotal, int total) {
+        double mul = ((double) attributeTotal / total);
+
+        double currentEntropy = Math.log(attributeTotal / (double) total) / Math.log(2);
+
+        return currentEntropy * mul * -1;
+    }
 
 }
