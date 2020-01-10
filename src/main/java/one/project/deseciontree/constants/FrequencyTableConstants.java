e734f9f9d8b38b27f1284ff8162ae6c71d4b765e
safinAyah <ayah.safeen@gmail.com>
2020-01-10 18:36:05 +0200

    ENTROPY IS DONEEEEEEEEE :D :D :D

---
+++ src/main/java/one/project/deseciontree/constants/FrequencyTableConstants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package one.project.deseciontree.constants;
+
+/**
+ *
+ * @author ayah
+ */
+public class FrequencyTableConstants {
+
+    public static final int TOTAL_INDEX = 2;
+    public static final int POISINAS_iNDEX = 1;
+    public static final int EDABLE_iNDEX = 0;
+    public static final double POISINAS_VALUE = 1.0;
+    public static final double EDABLE_VALUE= 0.0;
+    public static final String FILE_NAME = "/home/ayah/test.arff";
+
+}
